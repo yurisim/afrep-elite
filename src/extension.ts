629eddbd6b7138c7c1ec5f3d4ed8331165276d70
Yuri Sim <edaraks@gmail.com>
2024-11-14 03:32:03 -0800

    Enhance ELITE Generator: update generated text format and add stimulus input fields in webview

--- src/extension.ts
+++ src/extension.ts
@@ -25,7 +25,7 @@ class EliteGeneratorViewProvider implements vscode.WebviewViewProvider {
                 switch (message.command) {
                     case 'generate':
                         const strings = message.data.map((item: any) => {
-                            return `${item.component} should have between ${item.rangeStart}${item.unit1} and ${item.rangeEnd}${item.unit2}`;
+                            return `${item.component} => A,INIT,DCS,${item.stimulus}${item.stimulusUnit},>${item.rangeStart}${item.unit1},<${item.rangeEnd}${item.unit2},0.0T,0.1S`;
                         });
                         webviewView.webview.postMessage({
                             command: 'showResult',
@@ -45,7 +45,7 @@ class EliteGeneratorViewProvider implements vscode.WebviewViewProvider {
 
 export function activate(context: vscode.ExtensionContext) {
     const provider = new EliteGeneratorViewProvider(context.extensionUri);
-    
+
     context.subscriptions.push(
         vscode.window.registerWebviewViewProvider(EliteGeneratorViewProvider.viewType, provider)
     );
@@ -85,6 +85,9 @@ function getWebviewContent() {
                         max-width: 100%;
                         box-sizing: border-box;
                     }
+                    .input-container {
+                        width: 75px;
+                    }
                     .component-row {
                         display: flex;
                         align-items: center;
@@ -106,7 +109,7 @@ function getWebviewContent() {
                     
                     <div style="margin-top: 20px;">
                         <h3 style="color: var(--vscode-foreground);">Generated Text:</h3>
-                        <textarea id="result" rows="10" style="width: 100%;" readonly></textarea>
+                        <textarea id="result" rows="10" style="width: 100%;"></textarea>
                     </div>
                 </div>
 
@@ -124,13 +127,20 @@ function getWebviewContent() {
                             <select id="component_\${componentCount}" class="vscode-select">
                                 \${components.map(c => \`<option value="\${c}">\${c}</option>\`).join('')}
                             </select>
-                            <input type="number" id="range1_\${componentCount}" min="1" max="10" value="1" class="vscode-input" />
+                            <input type="number" id="stimulus_\${componentCount}" placeholder="Stimulus" value="0.1" class="input-container"/>
+                            <select id="stimulusUnit_\${componentCount}" class="vscode-select">
+                                <option value="A" selected>A</option>
+                                <option value="mA">mA</option>
+                                <option value="V">V</option>
+                                <option value="mV">mV</option>
+                            </select>
+                            <input type="number" id="range1_\${componentCount}" min="0" max="100" value="1" class="vscode-input" />
                             <select id="unit1_\${componentCount}" class="vscode-select">
                                 <option value="ohm">ohm</option>
                                 <option value="kohm">kohm</option>
                                 <option value="Mohm">Mohm</option>
                             </select>
-                            <input type="number" id="range2_\${componentCount}" min="1" max="10" value="10" class="vscode-input" />
+                            <input type="number" id="range2_\${componentCount}" min="0.05" max="100" value="10" class="vscode-input" />
                             <select id="unit2_\${componentCount}" class="vscode-select">
                                 <option value="ohm">ohm</option>
                                 <option value="kohm">kohm</option>
@@ -155,6 +165,8 @@ function getWebviewContent() {
                             const id = form.id.split('_')[1];
                             data.push({
                                 component: document.getElementById(\`component_\${id}\`).value,
+                                stimulus: document.getElementById(\`stimulus_\${id}\`).value,
+                                stimulusUnit: document.getElementById(\`stimulusUnit_\${id}\`).value,
                                 rangeStart: document.getElementById(\`range1_\${id}\`).value,
                                 rangeEnd: document.getElementById(\`range2_\${id}\`).value,
                                 unit1: document.getElementById(\`unit1_\${id}\`).value,
